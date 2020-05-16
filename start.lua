2764e03ceb1149e1f3f783852b39344a3b8b6bb2
KaizerFox <29178963+KaizerFox@users.noreply.github.com>
2020-05-16 13:59:26 -0400

    Create start.lua

---
+++ start.lua
@@ -0,0 +1,4 @@
+ if game.Players.LocalPlayer.Character == nil then repeat wait() until game.Players.LocalPlayer.Character ~= nil end   
+                    if not game.Players.LocalPlayer.Character:FindFirstChild("Head") then repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("Head") wait(3) end
+                    if game.Players.LocalPlayer.Character == nil then return; end
+                    if not game.Players.LocalPlayer:FindFirstChild("HumanoidRootPart") then repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") end
